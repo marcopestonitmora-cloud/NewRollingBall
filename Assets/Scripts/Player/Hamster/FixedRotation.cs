2c9fd03cde4b54fd31643c7e5925541f6cef5ff5
marcopestonitmora-cloud <marcopestonitmora@gmail.com>
2026-03-18 19:22:22 +0100

    Mouse Movement/Jump/Camera

--- Assets/Scripts/Player/Hamster/FixedRotation.cs
+++ Assets/Scripts/Player/Hamster/FixedRotation.cs
@@ -2,23 +2,24 @@ using UnityEngine;
 
 public class FixedRotation : MonoBehaviour
 {
-    [SerializeField] private Transform ball;  // reference to the ball
+    [SerializeField] private Transform HamsterPosition;  // reference to the empty
+    [SerializeField] private Vector3 offset = new Vector3(0, -0.2f, 0);
     [SerializeField] private float rotationSpeed = 10f; // smooth rotation speed
 
     private Vector3 lastBallPosition;
 
     void Start()
     {
-        lastBallPosition = ball.position;
+        lastBallPosition = HamsterPosition.position;
     }
 
     void LateUpdate()
     {
         //Keep Position
-        transform.position = ball.position;
+        transform.position = HamsterPosition.position + offset;
 
         // calculate ball movement direction
-        Vector3 moveDirection = ball.position - lastBallPosition;
+        Vector3 moveDirection = HamsterPosition.position - lastBallPosition;
         moveDirection.y = 0f; 
         
         if (moveDirection.magnitude > 0.01f)
@@ -28,6 +29,6 @@ public class FixedRotation : MonoBehaviour
         }
 
         // Update to the new position
-        lastBallPosition = ball.position;
+        lastBallPosition = HamsterPosition.position;
     }
 }
--- Assets/Scripts/Player/PlayerMovementSystem.cs
+++ Assets/Scripts/Player/PlayerMovementSystem.cs
@@ -8,6 +8,12 @@ public class PlayerMovementSystem : MonoBehaviour
     
     [Header("Movement")]
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float JumpForce = 7f;
+    private bool IsGrounded;
+    private float maxRayDistance = 1.1f;
+    
+    [Header("Camera")]
+    [SerializeField] private Transform cameraTransform;
     
     private void Awake()
     {
@@ -23,20 +29,46 @@ public class PlayerMovementSystem : MonoBehaviour
     
     void Update()
     {
-        
+        //The player’s movement input is modified based on the facing camera direction
+
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        movement = (forward * vInput + right * hInput).normalized;
+
+        if (Physics.Raycast(transform.position, Vector3.down, maxRayDistance))
+        {
+            IsGrounded = true;
+        }
+
+        if (IsGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            IsGrounded = false;
+            Jump();
+        }
     }
 
     private void FixedUpdate()
     {
-        float hInput = Input.GetAxis("Horizontal");
-        float vInput = Input.GetAxis("Vertical");
-        
-        movement = new Vector3(hInput, 0, vInput).normalized;
         PlayerMovement();
     }
 
-    public void PlayerMovement() //Add Forces to move the player
+    private void PlayerMovement()
     {
         main.rb.AddForce(movement * speed, ForceMode.Force);
+    }
+
+    private void Jump()
+    {
+        main.rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
     }
 }
