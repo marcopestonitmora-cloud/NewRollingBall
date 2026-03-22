312efb7cf7cc96ec4964f6e34cb5696062a6340f
marcopestonitmora-cloud <marcopestonitmora@gmail.com>
2026-03-22 13:41:59 +0100

    MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] public AudioSource audioSource;
+    [SerializeField] private AudioClip playButton;
+    [SerializeField] private AudioClip exitButton;
+
+    public void Play()
+    {
+        AudioManager.instance.PlaySound(playButton,1f);
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        AudioManager.instance.PlaySound(exitButton,1f);
+        Debug.Log("Closing the game...");
+        Application.Quit();
+    }
+}
